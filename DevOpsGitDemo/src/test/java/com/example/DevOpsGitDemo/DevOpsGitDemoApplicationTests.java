c07a3cbe505e07d0295ad5016a2b1592c3248b52
Shweta Shrikant Chalke <shwetachalke731@gmail.com>
2021-11-03 16:53:34 +0530

    Adding Springboot demo project

---
+++ DevOpsGitDemo/src/test/java/com/example/DevOpsGitDemo/DevOpsGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOpsGitDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
